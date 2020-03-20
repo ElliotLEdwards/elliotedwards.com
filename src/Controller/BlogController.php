5c33834f50b084c403a454d52ed774291b61cc62
Elliot Edwards <elliotedwards@elliots-macbook-pro.home>
2020-03-20 01:16:11 +0100

    Tried receiving node variables in symfony controller

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\BrowserKit\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends AbstractController
@@ -10,8 +11,10 @@ class BlogController extends AbstractController
     /**
      * @Route("/blog", name="blog_list")
      */
-    public function list()
+    public function list(Request $request)
     {
+        $title = $request->attributes->get('access_token');
+        $title = $request->attributes->get('refresh_token');
         $var = "elliotedwards.com first commit !";
         return $this->render('base.html.twig', [
             "myVariable" => $var,
